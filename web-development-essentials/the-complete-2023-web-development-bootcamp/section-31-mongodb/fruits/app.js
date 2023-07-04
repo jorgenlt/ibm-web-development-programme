5a5c2d46c02547c1011e517228c40cc735e9a51f
Joergen Tjoennteig <jorgenlt@outlook.com>
2023-07-04 18:37:29 +0200

    section-32

--- web-development-essentials/the-complete-2023-web-development-bootcamp/section-31-mongodb/fruits/app.js
+++ web-development-essentials/the-complete-2023-web-development-bootcamp/section-31-mongodb/fruits/app.js
@@ -1,9 +1,16 @@
 const mongoose = require('mongoose');
-mongoose.connect('mongodb://localhost:27017/fruitsDB');
+mongoose.connect('mongodb://localhost:27017/fruitsDB', { useNewUrlParser: true, useUnifiedTopology: true });
 
 const fruitSchema = new mongoose.Schema({
-  name: String,
-  rating: Number,
+  name: {
+    type: String,
+    required: [true, 'Please type in a name.']
+  },
+  rating: {
+    type: Number,
+    min: 1,
+    max: 10
+  },
   review: String
 });
 
@@ -31,22 +38,52 @@ const person = new Person({
 
 person.save();
 
-const kiwi = new Fruit({
-  name: 'kiwi',
-  rating: 9,
-  review: 'Fine.'
-});
+// const kiwi = new Fruit({
+//   name: 'kiwi',
+//   rating: 9,
+//   review: 'Fine.'
+// });
 
-const banana = new Fruit({
-  name: 'banana',
-  rating: 5,
-  review: 'ok.'
-});
+// const banana = new Fruit({
+//   name: 'banana',
+//   rating: 5,
+//   review: 'ok.'
+// });
 
-const orange = new Fruit({
-  name: 'orange',
-  rating: 1,
-  review: 'bad.'
-})
+// const orange = new Fruit({
+//   name: 'orange',
+//   rating: 1,
+//   review: 'bad.'
+// })
 
-Fruit.insertMany([kiwi, banana, orange]);
+// Fruit.insertMany([kiwi, banana, orange]);
+
+// Fruit.find().exec()
+//   .then(fruits => {
+//     fruits.forEach(fruit => console.log(fruit.name));
+//   })
+//   .catch(error => {
+//     console.log(error);
+//   })
+
+// const ObjectId = mongoose.Types.ObjectId;
+// const fruitId = new ObjectId("64a3fe25332dd563de3a363e");
+
+// Fruit.updateOne({ _id: fruitId }, { name: 'peach' })
+//   .then(result => {
+//     console.log('Update successfull.');
+//   })
+//   .catch(error => {
+//     console.log(error);
+//   });
+
+// Fruit.deleteOne({ name: 'kiwi' })
+//   .then(result => {
+//     console.log('Item deleted.');
+//   })
+//   .catch(error => {
+//     console.log(error);
+//   });
+
+
+
